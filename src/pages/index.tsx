a5b0250f5f8c07d239f9a11db6fe7eda2bb4c381
Carvalho2019 <taisomcarvalho15@gmail.com>
2022-05-15 16:09:25 +0100

    created struct of application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Head from "next/head";
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>          
+        <title>Inicio | ig.news</title>
+      </Head>
       <h1>Hello World </h1>
-    </div>
+    </>
   );
 }
